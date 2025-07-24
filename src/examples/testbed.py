41c85a92b5307506266636e7cfbcfd3c8d01b43e
Bax <iraluqbond@gmail.com>
2025-07-24 09:14:30 -0800

    syncing

--- src/examples/testbed.py
+++ src/examples/testbed.py
@@ -1,7 +1,7 @@
 import migrids_lite as mlt
 import pandas as pd
 from random import randrange
-import timeit
+# import timeit
 
 # QOL for printing
 pd.set_option('display.max_columns', None)
@@ -13,8 +13,8 @@ pd.set_option('display.width', None)
 # elec_load = mlt.GetElectricLoad(r'..\testbed_load.txt')
 # print(elec_load.data)
 
-testload = [randrange(120, 800) for x in range(0, 8760)]
-testresource = [randrange(200, 800) if x < 4000 else 0 for x in range(0, 8760)]
+testload = [randrange(120, 1000) for x in range(0, 40)]
+testresource = [randrange(200, 500) if x < 4000 else 0 for x in range(0, 40)]
 #
 #
 e_loads = mlt.EnergyType.EnergyType('electric_load', pd.DataFrame(testload))
@@ -26,26 +26,30 @@ resources = mlt.EnergyType.EnergyType('resource', pd.DataFrame(testresource))
 e_in = mlt.EnergyInputs.EnergyInputs(e_loads, resources)
 
 fohundy = mlt.Generator.Generator('fohundy', 400, 0.30, {0.50: 14, 1.00: 28})
-fotoo = mlt.Generator.Generator('fotoo', 400, 0.30, {0.50: 14, 1.00: 28})
-pwrhouse = mlt.Powerhouse.Powerhouse((fohundy, fotoo))
+sixhundy = mlt.Generator.Generator('sixhundy', 600, 0.30, {0.50: 21, 1.00: 42})
+pwrhouse = mlt.Powerhouse.Powerhouse((fohundy, sixhundy))
 
-vrla = mlt.Storage.Storage('vrla', 100, 100, 1000, 0.3)
+vrla = mlt.Storage.Storage('vrla', 50, 100, 1000, 0.3)
 
-t1 = timeit.default_timer()
+gen_shifting = mlt.System.System(e_loads, pwrhouse, 's', vrla, resources)
 
-src = mlt.SrcLimits.SrcLimits(e_in, pwrhouse)
-# print(pwrhouse.combo_mol_caps)
-src.calc_all(vrla.rated_discharge)
+print(gen_shifting.fuel_usages.usages)
 
-testshifting = mlt.Timeshifting.Timeshift(vrla, src)
-testshifting.calc(batt_reset=0.3, residual_cutoff=0.1)
-testshifting.get_vitals()
+# t1 = timeit.default_timer()
 
-tanks = mlt.TankFarm.TankFarm(pwrhouse, testshifting.vitals)
-
-t2 = timeit.default_timer()
-
-print(t2-t1)
+# src = mlt.SrcLimits.SrcLimits(e_in, pwrhouse)
+# # print(pwrhouse.combo_mol_caps)
+# src.calc_all(vrla.rated_discharge)
+#
+# testshifting = mlt.Timeshifting.Timeshift(vrla, src)
+# testshifting.calc(batt_reset=0.3, residual_cutoff=0.005)
+# testshifting.get_vitals()
+#
+# tanks = mlt.TankFarm.TankFarm(pwrhouse, testshifting.vitals)
+#
+# t2 = timeit.default_timer()
+#
+# print(t2-t1)
 # print(tanks.usages)
-print(testshifting.vitals)
+# print(testshifting.vitals)
 # print(tanks.totals)
