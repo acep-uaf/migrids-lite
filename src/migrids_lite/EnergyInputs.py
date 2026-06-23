6589a7027a9359a088d854e08e6fff64e6aa4930
Bax <iraluqbond@gmail.com>
2026-06-23 11:23:56 -0800

    added more tries for multicolumn load and resource

--- src/migrids_lite/EnergyInputs.py
+++ src/migrids_lite/EnergyInputs.py
@@ -22,8 +22,19 @@ class EnergyInputs:
                 except: raise Exception("Error: no load column found!")
 
         elif (first_input.energy_type == 'electric_load') and (second_input.energy_type == 'resource'):
-            self.resource['resource'] = second_input.data
-            self.electric_load['electric_load'] = first_input.data
+            if first_input.data.shape[1] == 1:
+                self.electric_load['electric_load'] = first_input.data
+            else:
+                try:
+                    self.electric_load['electric_load'] = first_input.data['electric_load']
+                except: raise Exception("Error: no load column found!")
+
+            if second_input.data.shape[1] == 1:
+                self.resource['resource'] = second_input.data
+            else:
+                try:
+                    self.resource['resource'] = second_input.data['resource']
+                except: raise Exception("Error: no resource column found!")
         else:
             raise Exception('Error: one input must be of type \'resource\' and the other must be of type '
                             '\'electric_load\'')
